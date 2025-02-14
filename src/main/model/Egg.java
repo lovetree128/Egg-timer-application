358cd9f8a653f4c5a425ba164332f63a348f8714
UBC Student <carlgao@students.cs.ubc.ca>
2025-02-14 02:03:54 -0800

    Divided assignTime method into several smaller methods

--- src/main/model/Egg.java
+++ src/main/model/Egg.java
@@ -2,21 +2,22 @@ package model;
 
 // Represents an egg with user input method of cooking a specified time of cooking.
 public class Egg {
-    final int BOILED_EGG_SOFT_TIME = 360;
-    final int BOILED_EGG_MEDIUM_TIME = 480;
-    final int BOILED_EGG_HARD_TIME = 600;
+    public static final int BOILED_EGG_SOFT_TIME = 360;
+    public static final int BOILED_EGG_MEDIUM_TIME = 480;
+    public static final int BOILED_EGG_HARD_TIME = 600;
 
-    final int FRIED_EGG_SOFT_TIME = 120;
-    final int FRIED_EGG_MEDIUM_TIME = 180;
-    final int FRIED_EGG_HARD_TIME = 240;
+    public static final int FRIED_EGG_SOFT_TIME = 120;
+    public static final int FRIED_EGG_MEDIUM_TIME = 180;
+    public static final int FRIED_EGG_HARD_TIME = 240;
 
-    final int SCRAMBLED_EGG_SOFT_TIME = 180;
-    final int SCRAMBLED_EGG_MEDIUM_TIME = 240;
-    final int SCRAMBLED_EGG_HARD_TIME = 300;
+    public static final int SCRAMBLED_EGG_SOFT_TIME = 180;
+    public static final int SCRAMBLED_EGG_MEDIUM_TIME = 240;
+    public static final int SCRAMBLED_EGG_HARD_TIME = 300;
 
     private String method;
     private int doneness;
     private int remainingTime;
+    private int cookIndex;
 
     // REQUIRES: method must not be an empty string and method can't be
     // any string other than "boiled", "fried" or "scrambled"
@@ -25,50 +26,81 @@ public class Egg {
     public Egg(String method, int doneness) {
         this.method = method;
         this.doneness = doneness;
+        this.cookIndex = 0;
         assignTime();
     }
 
-    public void assignTime() {
-        int cook = 0;
+    // MODIFIES: this
+    // EFFECTS: transforms the method and doneness to index
+    public void toIndex() {
         switch (method) {
             case "boiled":
-                cook += 10;
+                cookIndex += 10;
                 break;
             case "fried":
-                cook += 20;
+                cookIndex += 20;
                 break;
             case "scrambled":
-                cook += 30;
+                cookIndex += 30;
                 break;
         }
-        cook += doneness;
-        switch (cook) {
+        cookIndex += doneness;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: call methods to assign cooking time according to the index
+    public void assignTime() {
+        toIndex();
+        assignTimeForBoiled();
+        assignTimeForFried();
+        assignTimeForScrambled();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: assign time for the boiled method eggs
+    public void assignTimeForBoiled() {
+        switch (cookIndex) {
             case 11:
                 remainingTime = BOILED_EGG_SOFT_TIME;
-                break;
-            case 21:
-                remainingTime = FRIED_EGG_SOFT_TIME;
-                break;
-            case 31:
-                remainingTime = SCRAMBLED_EGG_SOFT_TIME;
                 break;
             case 12:
                 remainingTime = BOILED_EGG_MEDIUM_TIME;
                 break;
-            case 22:
-                remainingTime = FRIED_EGG_MEDIUM_TIME;
-                break;
-            case 32:
-                remainingTime = SCRAMBLED_EGG_MEDIUM_TIME;
-                break;
             case 13:
                 remainingTime = BOILED_EGG_HARD_TIME;
+                break;
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: assign time for the fried method eggs
+    public void assignTimeForFried() {
+        switch (cookIndex) {
+            case 21:
+                remainingTime = FRIED_EGG_SOFT_TIME;
+                break;
+            case 22:
+                remainingTime = FRIED_EGG_MEDIUM_TIME;
                 break;
             case 23:
                 remainingTime = FRIED_EGG_HARD_TIME;
                 break;
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: assign time for the scrambled method eggs
+    public void assignTimeForScrambled() {
+        switch (cookIndex) {
+            case 31:
+                remainingTime = SCRAMBLED_EGG_SOFT_TIME;
+                break;
+            case 32:
+                remainingTime = SCRAMBLED_EGG_MEDIUM_TIME;
+                break;
             case 33:
                 remainingTime = SCRAMBLED_EGG_HARD_TIME;
+                break;
         }
     }
 
@@ -78,15 +110,14 @@ public class Egg {
         remainingTime -= 1;
     }
 
+    // MODIFIES: this
+    // EFFECTS: check if the egg is done (remainingTime == 0)
     public boolean isDone() {
-        return remainingTime == 0;
+        return remainingTime <= 0;
     }
 
-    public String getMethod() {
-        return method;
-    }
-
-    public String getDoneness() {
+    // EFFECTS: gets the string version of doneness
+    public String getDonenessInString() {
         switch (doneness) {
             case 1:
                 return "soft";
@@ -99,8 +130,23 @@ public class Egg {
         }
     }
 
+    public String getMethod() {
+        return method;
+    }
+
+    public int getDoneness() {
+        return doneness;
+    }
+
     public int getRemainingTime() {
         return remainingTime;
     }
 
+    public int getCookIndex() {
+        return cookIndex;
+    }
+
+    public void setRemainingTime(int time) {
+        remainingTime = time;
+    }
 }
