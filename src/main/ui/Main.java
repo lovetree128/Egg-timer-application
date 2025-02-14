c4db479cd7baf1527ef2fd5901f6c291a95604fa
UBC Student <carlgao@students.cs.ubc.ca>
2025-02-14 02:04:43 -0800

    Changed Main class to fit the new way of starting the timer

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,9 +3,6 @@ package ui;
 public class Main {
     public static void main(String[] args) throws Exception {
         EggTimer myEggTimer = new EggTimer();
-        myEggTimer.addEgg("boiled", 1);
-        myEggTimer.addEgg("boiled", 2);
-        myEggTimer.addEgg("boiled", 3);
         myEggTimer.startTimer();
     }
 }
