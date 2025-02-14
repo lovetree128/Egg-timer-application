664520da6d9a410f145affb660e0173d01247f2f
UBC Student <carlgao@students.cs.ubc.ca>
2025-02-14 02:04:18 -0800

    Added user input handle methods

--- src/main/ui/EggTimer.java
+++ src/main/ui/EggTimer.java
@@ -2,34 +2,73 @@ package ui;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import model.Egg;
 
 public class EggTimer {
     private List<Egg> eggs;
+    private Scanner input;
 
-    // EFFECTS: constructs an empty list of eggs and 
-    // runs the timer application
+    // EFFECTS: constructs an empty list of eggs and
+    // a scanner to receive user input.
     public EggTimer() {
         eggs = new ArrayList<Egg>();
+        input = new Scanner(System.in);
     }
 
     // MODIFIES: this
-    // EFFECTS: 
+    // EFFECTS: starts the timer
     public void startTimer() {
+        chooseEgg();
         try {
             while (!isAllDone()) {
                 display();
                 timeReduce();
                 Thread.sleep(1000);
             }
-        } catch (InterruptedException e) {}
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("All eggs are cooked!");
+    }
+
+    public void chooseEgg() {
+        boolean start = false;
+        String type = null;
+        int doneness = 0;
+        while (!start) {
+            displayEggType();
+            type = input.next();
+            if (type.equals("start")) {
+                start = true;
+            } else {
+                displayDoneness();
+                doneness = input.nextInt();
+                handleCommand(type,doneness);
+            }
+        }
     }
 
     // EFFECTS: prints the time
     public void display() {
         System.out.println(String.join("\t", getEggMethods()));
         System.out.println(String.join("\t", toMinute()));
+    }
+
+    public void displayEggType() {
+        System.out.println("\nSelect from:");
+        System.out.println("\tb -> Boiled");
+        System.out.println("\tf -> Fried");
+        System.out.println("\ts -> Scrambled");
+        System.out.println("\tstart -> start the timer");
+    }
+
+    public void displayDoneness() {
+        System.out.println("\nSelect from:");
+        System.out.println("\t1 -> Soft");
+        System.out.println("\t2 -> Medium");
+        System.out.println("\t3 -> Hard");
     }
 
     // MODIFIES: this
@@ -54,12 +93,16 @@ public class EggTimer {
     public List<String> toMinute() {
         List<String> eggTimeMinute = new ArrayList<String>();
         for (Egg egg : eggs) {
-            int min = egg.getRemainingTime() / 60;
-            int sec = egg.getRemainingTime() % 60;
-            if (sec >= 0 && sec <= 9) {
-                eggTimeMinute.add(min + ":" + "0" + sec);
+            if (egg.isDone()) {
+                eggTimeMinute.add(egg.getMethod() + egg.getDonenessInString() + "is cooked!");
             } else {
-                eggTimeMinute.add(min + ":" + sec);
+                int min = egg.getRemainingTime() / 60;
+                int sec = egg.getRemainingTime() % 60;
+                if (sec >= 0 && sec <= 9) {
+                    eggTimeMinute.add(min + ":" + "0" + sec);
+                } else {
+                    eggTimeMinute.add(min + ":" + sec);
+                }
             }
         }
         return eggTimeMinute;
@@ -69,9 +112,23 @@ public class EggTimer {
     public List<String> getEggMethods() {
         List<String> eggNames = new ArrayList<String>();
         for (Egg egg : eggs) {
-            eggNames.add(egg.getMethod() + egg.getDoneness());
+            eggNames.add(egg.getMethod() + egg.getDonenessInString());
         }
         return eggNames;
+    }
+
+    public void handleCommand(String command,int doneness) {
+        switch (command) {
+            case "b":
+                addEgg("boiled", doneness);
+                break;
+            case "f":
+                addEgg("fried", doneness);
+                break;
+            case "s":
+                addEgg("scrambled", doneness);
+                break;
+        }
     }
 
     public void addEgg(String method, int doneness) {
