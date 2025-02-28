b0d5815ee63d016d0b81cc228f5891275a96873f
UBC Student <2189418066@qq.com>
2025-02-27 22:18:27 -0800

    Implemented JsonReader.java

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -20,21 +20,35 @@ public class JsonReader {
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String fileLocation) {
-
+        this.fileLocation = fileLocation;
     }
 
     // EFFECTS: reads eggs from file and returns it
-    public List<Egg> read() {
-
+    public List<Egg> read() throws IOException {
+        String jsonData = readFile(fileLocation);
+        JSONArray jsonArray = new JSONArray(jsonData);
+        return parseEggs(jsonArray);
     }
 
     // EFFECTS: reads source file as string and returns it
-    private String readFile(String fileLocation) {
+    private String readFile(String fileLocation) throws IOException {
+        StringBuilder contentBuilder = new StringBuilder();
+        try (Stream<String> stream = Files.lines(Paths.get(fileLocation), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
 
+        return contentBuilder.toString();
     }
 
     // EFFECTS: parses eggs from JSON array and return list of eggs
     private List<Egg> parseEggs(JSONArray jsonArray) {
-
+        List<Egg> eggs = new ArrayList<>();
+        for (Object object : jsonArray) {
+            JSONObject jsonObject = (JSONObject) object;
+            Egg egg = new Egg(jsonObject.getString("Method"), jsonObject.getInt("Doneness"));
+            egg.setRemainingTime(jsonObject.getInt("Remaining time"));
+            eggs.add(egg);
+        }
+        return eggs;
     }
 }
