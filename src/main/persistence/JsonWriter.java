f3aa6592466de385ae4829272d5b67b985ee25e3
UBC Student <2189418066@qq.com>
2025-02-27 17:36:36 -0800

    Specification of JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,9 +1,33 @@
 package persistence;
 
+import java.security.PublicKey;
+
+import org.json.JSONObject;
 
 // Reference: JsonWriter class in JsonSerializationDemo
 // Represents a writer that writes JSON representation of List of eggs to file
 public class JsonWriter {
     
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter() {
+        
+    }
 
+    // MODIFIES: this
+    // EFFECTS: opnens writer
+    public void open() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of eggs to file
+    public void write() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+
+    }
 }
