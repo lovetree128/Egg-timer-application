83c76802995651f9e56cc1c3111160040d97dbc3
UBC Student <2189418066@qq.com>
2025-02-27 22:19:47 -0800

    Implemented read and save in EggTimer.java

--- src/main/ui/EggTimer.java
+++ src/main/ui/EggTimer.java
@@ -1,27 +1,33 @@
 package ui;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import model.Egg;
+import persistence.JsonReader;
+import persistence.JsonWriter;
 
 // Adds the threads to a list. Run them together and handle user input to 
 // determine the type of the egg and add or stop the specific egg while running
 public class EggTimer {
+    private static final String FILE_LOCATION = "./data/eggs.json";
     private List<EggThread> eggThreads;
     private Scanner input;
-    private List<String> eggNames;
+    private List<Egg> eggs;
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
 
     // EFFECTS: constructs an empty list of eggs and
     // a scanner to receive user input.
     public EggTimer() {
         eggThreads = new ArrayList<EggThread>();
         input = new Scanner(System.in);
-        eggNames = new ArrayList<>();
-        for (EggThread thread : eggThreads) {
-            eggNames.add(thread.getEgg().getDisplayName());
-        }
+        eggs = new ArrayList<>();
+        jsonReader = new JsonReader(FILE_LOCATION);
+        jsonWriter = new JsonWriter(FILE_LOCATION);
     }
 
     // MODIFIES: this
@@ -45,7 +51,8 @@ public class EggTimer {
             displayEggType();
             type = input.next();
             input.nextLine();
-            if (!type.equals("b") && !type.equals("f") && !type.equals("s") && !type.equals("start")) {
+            if (!type.equals("b") && !type.equals("f") && !type.equals("s") && !type.equals("start")
+                    && !type.equals("l")) {
                 if (type.equals("q")) {
                     break;
                 }
@@ -63,17 +70,21 @@ public class EggTimer {
 
     // EFFECTS: throws error information to the user when input is not number
     public void handleException(String type) {
-        int doneness = 0;
-        try {
-            doneness = input.nextInt();
-            if (doneness >= 4 | doneness < 1) {
-                System.out.println("Please enter a valid number (1-3)");
-            } else {
-                handleCommand(type, doneness);
+        if (type.equals("l")) {
+            handleCommand(type, 0);
+        } else {
+            int doneness = 0;
+            try {
+                doneness = input.nextInt();
+                if (doneness >= 4 | doneness < 1) {
+                    System.out.println("Please enter a valid number (1-3)");
+                } else {
+                    handleCommand(type, doneness);
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter an integer");
+                input.nextLine();
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Please enter an integer");
-            input.nextLine();
         }
     }
 
@@ -104,6 +115,7 @@ public class EggTimer {
                 + "\n\tf -> Fried"
                 + "\n\ts -> Scrambled"
                 + "\n\tstart -> start the timer"
+                + "\n\tl -> load timers from file"
                 + "\n\tq -> Quit");
     }
 
@@ -128,11 +140,14 @@ public class EggTimer {
             case "s":
                 addEgg("scrambled", doneness);
                 break;
+            case "l":
+                readTimer();
+                break;
         }
     }
 
     // MODIFIES: this
-    // EFFECTS: adds eggs to eggslist
+    // EFFECTS: adds egg to eggThreads
     public void addEgg(String method, int doneness) {
         Egg egg = new Egg(method, doneness);
         eggThreads.add(new EggThread(egg));
@@ -142,17 +157,65 @@ public class EggTimer {
     // EFFECTS: handles the user input while the egg threads are running
     public void handleRunningInput() {
         while (!isAllDone()) {
-            System.out.println("Please enter the name of the timer you want to stop\nor\n"
-                    + "Enter 'show time' to see the remaining time of the timers\nor\n"
-                    + "Enter 'add' to add a new timer");
+            System.out.println("Select from:"
+                    + "\n\tName of the egg -> Stop the egg"
+                    + "\n\tshow time -> See the remaining time of the timers"
+                    + "\n\tadd -> Add new timer"
+                    + "\n\tsave -> save the current timers to file and stop"
+                    + "\n\tstop -> stop all the timers");
             String userInput = input.nextLine();
-            if (!userInput.equals("add")) {
-                stopAndShow(userInput);
-            } else {
+            if (userInput.equals("save")) {
+                saveTimer();
+                stopAll();
+                break;
+            } else if (userInput.equals("add")) {
                 addTimer(userInput);
+            } else if (userInput.equals("stop")) {
+                stopAll();
+                break;
+            } else {
+                stopAndShow(userInput);
             }
         }
         System.out.println("All eggs are cooked or the application is stopped by the user");
+    }
+
+    // MODIFIES: this
+    // EFFECTS: reads the timers from the file
+    public void readTimer() {
+        List<Egg> eggs = new ArrayList<>();
+        try {
+            eggs = jsonReader.read();
+            for (Egg egg : eggs) {
+                eggThreads.add(new EggThread(egg));
+            }
+            System.out.println("Loaded " + jsonReader.read().size() + " eggs from " + FILE_LOCATION);
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + FILE_LOCATION);
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: stops all the timer and the whole application
+    public void stopAll() {
+        for (EggThread eggThread : eggThreads) {
+            eggThread.stopThread();
+        }
+    }
+
+    // EFFECTS: saves the egg timers to file
+    public void saveTimer() {
+        for (EggThread eggThread : eggThreads) {
+            eggs.add(eggThread.getEgg());
+        }
+        try {
+            jsonWriter.open();
+            jsonWriter.write(eggs);
+            jsonWriter.close();
+            System.out.println("Saved all eggs to " + FILE_LOCATION);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + FILE_LOCATION);
+        }
     }
 
     // MODIFIES: this
@@ -167,7 +230,7 @@ public class EggTimer {
     }
 
     // MODIFIES: this
-    // EFFECTS: stops the timer or shows the remaining time of the timers
+    // EFFECTS: stops the timer
     public void stopAndShow(String userInput) {
         if (!userInput.equals("show time")) {
             for (EggThread eggThread : eggThreads) {
