050dd3f9dcf636dfa3a9fe4d0ae5e8281c530cef
UBC Student <2189418066@qq.com>
2025-02-27 17:32:30 -0800

    Added JsonReader and JsonWriter class

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,9 @@
+package persistence;
+
+
+// Reference: JsonWriter class in JsonSerializationDemo
+// Represents a writer that writes JSON representation of List of eggs to file
+public class JsonWriter {
+    
+
+}
