ea80cc0e4fce1e2a9a1282038b44a07035e48270
UBC Student <2189418066@qq.com>
2025-03-22 14:45:53 -0700

    Created two classes for UI

---
+++ src/main/ui/EggUI.java
@@ -0,0 +1,15 @@
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class EggUI extends JFrame {
+    private JPanel buttonPanel;
+    
+}
