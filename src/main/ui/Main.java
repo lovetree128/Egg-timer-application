2df9998906e150d7d2a5cfa6f48e57d49b68e959
UBC Student <2189418066@qq.com>
2025-03-22 19:26:46 -0700

    Added some comments for the console main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,6 @@
 package ui;
 
+// Start the console version of the timer
 public class Main {
     public static void main(String[] args) throws Exception {
         EggTimer myEggTimer = new EggTimer();
