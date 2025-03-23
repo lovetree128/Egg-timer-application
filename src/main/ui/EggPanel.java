c39f1ae09845f41f56c5071796be97df4ef65489
UBC Student <2189418066@qq.com>
2025-03-22 17:04:25 -0700

    Specification for methods

--- src/main/ui/EggPanel.java
+++ src/main/ui/EggPanel.java
@@ -1,7 +1,29 @@
 package ui;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
+// Represents the panel for displaying time.
 public class EggPanel extends JPanel {
-    
+    EggThread eggThread;
+    JFrame timerFrame;
+    JLabel timeLabel;
+
+    // EFFECTS: constructs a frame to display timer
+    public EggPanel(EggThread eggThread) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: update the displaying time according to the thread.
+    private void updateTimeLabel() {
+
+    }
+
+    // EFFECTS: get the display text for timer
+    private String getEggTimeText() {
+        return eggThread.getEgg().getDisplayName() + " " + eggThread.getEgg().getRemainingTimeInMinute();
+    }
 }
